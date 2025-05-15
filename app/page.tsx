a6c785deb720d559edf6db9741f245141f66f519
Aaryan Shrestha <133579553+ShresthaAaryan@users.noreply.github.com>
2025-05-15 13:47:46 +0545

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,13 @@ export default function Home() {
   return (
     <>
     <Head>
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-MCPMK4Z6');</script>
+      <!-- End Google Tag Manager -->
       <link rel="icon" href="/ic.ico" />
       <title>Aaryan Shrestha - Full Stack Developer</title>
       <meta name="description" content="Portfolio of Aaryan Shrestha, a Full Stack Developer specializing in React, Node.js, and modern web technologies." />
